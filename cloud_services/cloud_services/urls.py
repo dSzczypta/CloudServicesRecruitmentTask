00f424ef0535cc910d0c0011e2579c39c72e7fed
dSzczypta <d.szczypta@gmail.com>
2024-03-14 22:30:44 +0100

    Add swagger, redoc
    add swagger and redoc documentation

--- cloud_services/cloud_services/urls.py
+++ cloud_services/cloud_services/urls.py
@@ -16,18 +16,39 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Appic Recrutment Task",
+        default_version='v1',
+        description="Appic Recrutment Task",
+        contact=openapi.Contact(email="d.szczypta@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    
+
     path('api/', include('restaurant.urls')),
-]
+
+    # Add Swagger schema view
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # Add ReDoc view
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
